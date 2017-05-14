0fbfe6065111f646e586b0bc936ac0507f4356bd
jiwenxing <jiwenxing@jd.com>
2017-05-14 19:43:52 +0800

    process date

--- app.js
+++ app.js
@@ -44,41 +44,40 @@ app.post('/saveComments',function(req, res){
 });
 
 app.get('/getComments', function(req, res){
-   // 获取用户名
+   
     var user_name=req.query.user;
-    console.log("user_name="+user_name);
     var filePathName = 'data/'+user_name+'.json'
 
     fs.readFile(filePathName,'utf8',function (err, data) {
         if(err) console.log(err);
         var json_str = '['+data+']';
-        // try {
+        try {
             json_str = json_str.replace(/\\/g,"");
-            // console.log("json_str="+json_str);
             json = JSON.parse(json_str);
-            // console.log(json);
             var comment_html = "";
             var length = json.length > 5 ? json.length : 5;
-            // console.log("json.length="+json.length+", length="+length)
             for(var i=json.length-1; i>=length-5; i--){
-              console.log("--------index="+i);
                var article_title = json[i].title; 
                var article_url = json[i].url;
                // console.log(article_url);
                var comments = json[i].comments;
                // console.log(comments);
                var comment_time = comments[0].ctime;
+               var now = new Date(comment_time);
+               var month=now.getMonth()+1; 
+               var date=now.getDate();
+               comment_time = month+"月"+date+"日";
                var comment_user = comments[0].user.nickname;
                var comment_content = comments[0].content;
-               comment_html = comment_html+'<li class="ds-comment"><div class="ds-meta"><a rel="nofollow author" target="_blank" href="">'+comment_user+'</a><span class="ds-time">'+comment_time+'</span></div><div class="ds-thread-title">在 <a href="'+article_url+'#comments">'+article_title+'</a> 中评论</div><div class="ds-excerpt">'+comment_content+'</div></li>';
+               comment_html = comment_html+'<li class="ds-comment"><div class="ds-meta"><a rel="nofollow author" target="_blank" href="">'+comment_user+' </a><span class="ds-time">'+comment_time+'</span></div><div class="ds-thread-title">在 <a href="'+article_url+'#comments">'+article_title+'</a> 中评论</div><div class="ds-excerpt">'+comment_content+'</div></li>';
             }
             
             res.header('Access-Control-Allow-Origin', '*');
             res.send({"status":"ok","content":comment_html});
-         // } catch (err) {
-         //    json = null;
-         //    res.send({"status":"error"});
-         // }
+         } catch (err) {
+            json = null;
+            res.send({"status":"error"});
+         }
 
      });
   
